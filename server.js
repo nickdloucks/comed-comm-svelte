87a7e8e5a5bc121b63fcdd03f12f653c3d66975c
Nick <nickdloucks@outlook.com>
2022-04-21 17:04:15 -0700

    server up and running, no CORS yet

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const port = process.env.PORT || 3002;
 
 server.use(express.static('public'));
 server.get('*', (req, res)=>{
+    console.log(__dirname);
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
-
-
+console.log(__dirname);
 
 server.listen(port, ()=>{
     console.log(`CoMedical Commissions app is listening for requests at http://localhost:${port}.`)
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import App from './App.svelte';
-import path from 'path';
-import express from 'express';
 
 const app = new App({
 	target: document.body,
@@ -9,21 +7,5 @@ const app = new App({
 	}
 });
 
-
-
-const server = express();
-const port = 3002;
-
-server.use(express.static('public'));
-server.get('*', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
-
-console.log("HELLO FROM Main.TS");
-
-
-server.listen(port, ()=>{
-    console.log(`CoMedical Commissions app is listening for requests at http://localhost:${port}.`)
-});
-
 export default app;
+
