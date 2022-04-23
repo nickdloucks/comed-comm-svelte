32febc850f7241260204fc5c1f6bc0132ef42946
Nick <nickdloucks@outlook.com>
2022-04-22 18:34:11 -0700

    better detail for types for DIR %s

--- src/stores/manufacturers-store.ts
+++ src/stores/manufacturers-store.ts
@@ -3,9 +3,15 @@ import { writable } from 'svelte/store';
 let sat_mfrs: string[] = ['GEN', 'MA', 'MET', 'PMBS', 'SCI', 'SKY', 'SPC', 'WEG'];
 let iit_mfrs: string[] = ['ARG', 'AXI', 'CON', 'MED', 'MRT', 'NOZ', 'TID', 'RAD'];
 
-type SAT_pcts = {
-
+export type MFR_pct = {
+    mfr_id: string;
+    pct: number;
 }
-type IIT_pcts = {
-    
-}
+export let IIT_pcts: Array<MFR_pct> = iit_mfrs.map((mfr): MFR_pct =>{
+    let mfrPct:MFR_pct = {mfr_id: mfr, pct: 0.0};
+    return mfrPct;
+});
+export let SAT_pcts: Array<MFR_pct> = sat_mfrs.map((mfr): MFR_pct =>{
+    let mfrPct:MFR_pct = {mfr_id: mfr, pct: 0.0};
+    return mfrPct;
+});
--- src/stores/rep-store.ts
+++ src/stores/rep-store.ts
@@ -1,11 +1,13 @@
 import { writable } from 'svelte/store';
+import type { MFR_pct } from './manufacturers-store';
+import { IIT_pcts, SAT_pcts } from './manufacturers-store';
 
 export type SalesRep = {
   rep_name: string;
   territories: string[];
   sales_team: string[];
   otd_pct: string | number;
-  dir_pcts: unknown;
+  dir_pcts: Array<MFR_pct>;
   base_salary: number;
   monthly_draw: number;
 };
@@ -15,7 +17,7 @@ export const currentRep = writable<SalesRep>({
   territories: ['1020', '1021'],
   sales_team: ['IIT', 'SAT'],
   otd_pct: 1.00,
-  dir_pcts: {ALL: 1.00},
+  dir_pcts: [...IIT_pcts, ...SAT_pcts],
   base_salary: 0,
   monthly_draw: 0
 });
