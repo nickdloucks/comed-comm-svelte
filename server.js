104737c3e51d20f26b88a5605406ab0610d90b14
Nick <nickdloucks@outlook.com>
2022-04-23 19:53:48 -0700

    configure __filename  & __dirname for ES modules

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
+import {dirname}  from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
