5614e10610e0256a59082f1df0c87abaeba6fe12
Nick <nickdloucks@outlook.com>
2022-04-27 14:58:17 -0700

    init OTD calculator module

---
+++ src/utilities/otd-calc.ts
@@ -0,0 +1,6 @@
+
+
+export const OTD_CALC = function(profits: Array<number>, profitp_cts: Array<number>, prod_group_ids: Array<string>): number{
+    let tot_OTD_com = 0; // total commissions on "OTDect" sales
+    return tot_OTD_com;
+}
