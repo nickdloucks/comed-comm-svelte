ff7511ef718fce35ef20171e2775b9eca57275e6
Nick <nickdloucks@outlook.com>
2022-04-27 16:05:14 -0700

    init specs for DIR and OTD calculators

---
+++ src/tests/dir-calcSpec.ts
@@ -0,0 +1,5 @@
+import { DIR_CALC } from "../utilities/dir-calc";
+
+describe('A commissions calculation tool for sales made "directly" from manufacturer.', ()=>{
+    
+})
---
+++ src/tests/otd-calcSpec.ts
@@ -0,0 +1,5 @@
+import { OTD_CALC } from "../utilities/otd-calc";
+
+describe('A commissions calculation tool for sales "out-the-door".', ()=>{
+
+})
