2d065fd9e6724ebf4a11d2cb7f7bab90b0ce2757
Nick <nickdloucks@outlook.com>
2022-04-27 14:39:00 -0700

    init file for DIR commissions calculator

---
+++ src/utilities/dir-calc.ts
@@ -0,0 +1,6 @@
+
+
+export const DIR_CALC = function(tracings: Array<number>, prod_group_ids: Array<string>): number{
+    let tot_DIR_com = 0; // total commissions on "Direct" sales
+    return tot_DIR_com;
+}
