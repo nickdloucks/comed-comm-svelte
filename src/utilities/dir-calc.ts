d017d92f3506d3bd2585d1c02cd7530aa3550f7a
Nick <nickdloucks@outlook.com>
2022-04-28 09:45:17 -0700

    add docs for claculators

--- src/utilities/dir-calc.ts
+++ src/utilities/dir-calc.ts
@@ -1,4 +1,10 @@
-
+/**
+ * A calculator to determine how much money in commissions should be paid to a given sales rep 
+ * based on a given list of sales, specifically sales that were shipped directly from the maunfacturer to the customer.
+ * @param profits : Array<number>; Represents CoMedical's gross profit made on each sale.
+ * @param prod_group_ids : Array<string>; String abbreviations representing the manufactuere of a given product for a sale.
+ * @returns : number; Total commissions $ to be paid to the rep for the given list of "direct" sales tracings.
+ */
 
 export const DIR_CALC = function(profits: Array<number>, prod_group_ids: Array<string>): number{
     let tot_DIR_com = 0; // total commissions on "Direct" sales
--- src/utilities/otd-calc.ts
+++ src/utilities/otd-calc.ts
@@ -1,6 +1,13 @@
+/**
+ * A calculator to determine how much money in commissions should be paid to a given sales rep 
+ * based on a given list of sales, specifically sales that were shipped out CoMedical's warehouse to the customer.
+ * @param profits : Array<number>; Dolar amounts representing CoMedical's gross profit made on each sale.
+ * @param profit_pcts : Array<number>; Percentage amounts representing CoMedical's gross profit on each sale.
+ * @param prod_group_ids : Array<string>; String abbreviations representing the manufactuere of a given product for a sale.
+ * @returns : number; Total commissions $ to be paid to the rep for the given list of "out-the-door" sales tracings.
+ */
 
-
-export const OTD_CALC = function(profits: Array<number>, profitp_cts: Array<number>, prod_group_ids: Array<string>): number{
+export const OTD_CALC = function(profits: Array<number>, profit_pcts: Array<number>, prod_group_ids: Array<string>): number{
     let tot_OTD_com = 0; // total commissions on "OTDect" sales
     return tot_OTD_com;
 }
