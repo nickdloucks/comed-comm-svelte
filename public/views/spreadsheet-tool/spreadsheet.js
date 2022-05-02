51d12fb432393f7cdf64e364546fabdc937bc150
Nick <nickdloucks@outlook.com>
2022-05-02 16:19:54 -0700

    config headers to match PIR

--- public/views/spreadsheet-tool/spreadsheet.js
+++ public/views/spreadsheet-tool/spreadsheet.js
@@ -1,19 +1,50 @@
 var columnDefs = [
-    {headerName: 'Make', field: 'make'},
-    {headerName: 'Model', field: 'model'},
-    {headerName: 'Price', field: 'price'}
+    // {headerName: 'Make', field: 'make'},
+    // {headerName: 'Model', field: 'model'},
+    // {headerName: 'Price', field: 'price'},
+
+
+    {field: 'invoice_date', headerName: 'invoice_date', },
+    {field: 'ship2_name', headerName: 'ship2_name', },
+    {field: 'ship2_postal_code', headerName: 'ship2_postal_code', },
+    {field: 'period', headerName: 'period', },
+    {field: 'year_for_peroid', headerName: 'year_for_peroid', },
+    {field: 'qty_shipped', headerName: 'qty_shipped', },
+    {field: 'item_id', headerName: 'item_id', },
+    {field: 'extended_price', headerName: 'extended_price', },
+    {field: 'commission_cost', headerName: 'commission_cost', },
+    {field: 'product_group_id', headerName: 'product_group_id', },
+    {field: 'salesrep_id', headerName: 'salesrep_id', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', },
+    // {field: '', headerName: '', }
 ];
 
-var rowData =[
-    {make: 'Dodge', model: 'Challenger', price: 37000},
-    {make: 'Ford', model: 'Mustang', price: 36000},
-    {make: 'Chevy', model: 'Camaro', price: 34000},
-];
 
-var gridOptions ={
+const gridOptions = {
     columnDefs: columnDefs,
-    rowData: rowData
-}
+    defaultColDef: {
+        sortable: true,
+        filter: true,
+        resizeable: true,
+        minWidth: 80,
+        flex: 1
+    },
+    rowSelection: 'multiple',
+    rowData: [
+        {fred: '1', make: 'Dodge', model: 'Challenger', price: 37000},
+        {fred: '2', make: 'Ford', model: 'Mustang', price: 36000},
+        {fred: '3', make: 'Chevy', model: 'Camaro', price: 34000},
+    ]
+};
 
 var eGridDiv = document.querySelector('#myGrid');
 
