7f668c91046d4ed9af89a2d1160be86a3cc7a887
Nick <nickdloucks@outlook.com>
2022-05-03 12:52:42 -0700

    tidy up MFR % config part of rep-config UI

--- src/components/rep-configUI.svelte
+++ src/components/rep-configUI.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { currentRep } from '../stores/rep-store';
-    // import RepSelect from './rep-select.svelte';
+    import RepSelect from './rep-select.svelte';
     
     const update_territories = function(val: unknown): void{
         $currentRep.territories
@@ -14,23 +14,27 @@
 </script>
 
 <section>
-    <!-- <svelte:component this={RepSelect} id='repConfigUI-select'/> -->
+    <svelte:component this={RepSelect} id='repConfigUI-select'/>
     <h4>{$currentRep.rep_name}</h4>
     <p>Territories:</p>
     {#each $currentRep.territories as territory}
-        <input class='territory-config' type="text" placeholder={territory} value={territory}/><br>
+        <input class='territory-config' type="text" placeholder={String(territory)} value={territory}/><br>
     {/each}
     <label for='salary'>Base Salary: &nbsp;</label><input name="salary" type='number' placeholder={'$'.concat(String($currentRep.base_salary))}><br>
     <p>DIR %</p>
     <ul>
         {#each $currentRep.dir_pcts as mfr}
-            <li>{mfr.mfr_id}: &nbsp; {mfr.pct}%</li>
+            <li class="MFR-list"><label for={mfr.mfr_id.concat('-list-item')}>{mfr.mfr_id}:</label> &nbsp; 
+                <input name={mfr.mfr_id.concat('-list-item')} type="number" placeholder={String(mfr.pct)}> &nbsp; %</li>
         {/each}
     </ul>
     <button class="good-button" on:click={saveRep}>Save Changes</button>
 </section>
 
 <style>
+    .MFR-list{
+        font-weight: bold;
+    }
     .territory-config{
         width: fit-content;
         color: #8b0000;
