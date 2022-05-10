86e16bf0fd039da24fd60a0ac9bd4a316634984e
Nick <nickdloucks@outlook.com>
2022-05-10 13:19:27 -0700

    buttons get border and bkgd color transition

--- src/components/file-upload.svelte
+++ src/components/file-upload.svelte
@@ -4,7 +4,16 @@
 
 </script>
 
-<form>
+<form class="sales-upload">
     <label for={salesType.concat('-tracings-upload')}>Upload {salesType} Sales Tracings: &nbsp;</label>
-    <input id={salesType.concat('-tracings-upload')} name={salesType.concat('-tracings-upload')} type="file" accept=".csv .xlsx">
+    <input class="neutral-button" id={salesType.concat('-tracings-upload')} name={salesType.concat('-tracings-upload')} type="file" accept=".csv .xlsx">
 </form>
+
+<style>
+    label{
+        font-weight: bold;
+    }
+    .sales-upload{
+        width: max-content;
+    }
+</style>
