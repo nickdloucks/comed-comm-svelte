5f72fcabe6497dc67252e836b15a9b53c91de77b
Nick <nickdloucks@outlook.com>
2022-05-12 10:26:18 -0700

    redo MFR pcts data types

--- src/utilities/types/comedical-types.d.ts
+++ src/utilities/types/comedical-types.d.ts
@@ -1,3 +1,5 @@
+import { iit_mfrs, sat_mfrs } from "../../stores/manufacturers-store";
+
 enum Territories {
   '1002' = 2, 
   '1003' = 3, 
@@ -14,9 +16,38 @@ enum Territories {
   '1021' = 21
 }
 
-declare interface MFR_pct { // maps a manufacturer abbreviation (aka product group id) to a percentage
-    mfr_id: string;
-    pct: number;
+// declare interface MFR_pct { // maps a manufacturer abbreviation (aka product group id) to a percentage
+//   // mfr_id: string;
+//   // pct: number;
+//   [key: string]: number;
+// }
+
+
+class SAT_pcts{
+  [key: string]: number;
+  constructor(sat_mfrs: string[]){
+    for (let i = 0; i < sat_mfrs.length; i++){ // For each manufacturer in the list:
+      this[sat_mfrs[i]]; // initialize a key with the name as the string abbreviation and the value as a default of 20%
+    }
+  }
+}
+
+class IIT_pcts{
+  [key: string]: number;
+  constructor(iit_mfrs: string[]){
+    for (let i = 0; i < iit_mfrs.length; i++){ // For each manufacturer in the list:
+      this[iit_mfrs[i]]; // initialize a key with the name as the string abbreviation and the value as a default of 20%
+    }
+  }
+}
+
+class All_pcts{
+  [key: string]: number;
+  constructor([...iit_mfrs, ...sat_mfrs]: string[]){ // Combine both lists of MFRs into one array
+    for (let i = 0; i < arguments[0].length; i++){ // For each manufacturer in the list:
+      this[arguments[0][i]]; // initialize a key with the name as the string abbreviation and the value as a default of 20%
+    }
+  }
 }
 
 declare interface variableOTD { // sales reps can have varrying commission percentages for "out-the-door" sales
@@ -31,7 +62,7 @@ declare interface SalesRep { // configurable properties of a CoMedical Sales Rep
   territories: Array<Territories>;
   sales_team: string[];
   otd_pct: variableOTD | number;
-  dir_pcts: Array<MFR_pct>;
+  dir_pcts: SAT_pcts | IIT_pcts | All_pcts;
   base_salary: number;
   monthly_draw: number;
 };
@@ -57,16 +88,16 @@ enum Sale_period {
 }; // a sale period refers to a month of the year so the number much be from 1-12
 
 declare interface Sales_tracing{
-    end_user: string; // name of customer
-    zip_code: number | string; // used to determine which sales territory owns the sale
-    gross_profit: number; // reps get a certain percent of this dollar value as commission
-    gp_pct: number; // Gross Profit as a percentage of the Price
-    product_group_id: string; // abbreviation for the manufacturer that produces the product for a given instance of a sale sale
-    sales_rep: string;
-    item_id: string;
-    month_of_sale: Sale_period;
-    year_of_sale: number;
-    qty_shipped: number;
+  end_user: string; // name of customer
+  zip_code: number | string; // used to determine which sales territory owns the sale
+  gross_profit: number; // reps get a certain percent of this dollar value as commission
+  gp_pct: number; // Gross Profit as a percentage of the Price
+  product_group_id: string; // abbreviation for the manufacturer that produces the product for a given instance of a sale sale
+  sales_rep: string;
+  item_id: string;
+  month_of_sale: Sale_period;
+  year_of_sale: number;
+  qty_shipped: number;
 }
 
 declare interface DIR_tracing extends Sales_tracing{
@@ -74,9 +105,9 @@ declare interface DIR_tracing extends Sales_tracing{
 }
 
 declare interface OTD_tracing extends Sales_tracing{
-    sales_rep_id: number | string; // used to compare agains the rep's list of territiries. Could theoretically help catch errors in asssignment of sales to reps
-    commission_cost: number;
-    ext_com_cost: number; // cost to bring the product into CoMedical's warehouse: used to calc Gross Profit
-    ext_price: number; // Price paid by customer: used to cal GP $
-    ship_to_id: number | string; // unique identifier in P21 used to index customer locations
+  sales_rep_id: number | string; // used to compare agains the rep's list of territiries. Could theoretically help catch errors in asssignment of sales to reps
+  commission_cost: number;
+  ext_com_cost: number; // cost to bring the product into CoMedical's warehouse: used to calc Gross Profit
+  ext_price: number; // Price paid by customer: used to cal GP $
+  ship_to_id: number | string; // unique identifier in P21 used to index customer locations
 }
